87449504b9b692010c30efffde228fa1f47cf46f
OtabekDilshodivich <alfa98769@dmail.com>
2024-02-09 17:37:40 +0500

    Sneakers

--- basic/settings.py
+++ basic/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@8z+__#v*2kcw)1@47fqp+8+(pk+ul=0yf6oqxam10evpaz3)c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
